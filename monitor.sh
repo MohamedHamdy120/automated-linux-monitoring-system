a8181e7871d752f83bf35466409d37f71dc7d40e
Mohamed Hamdy <mohamedhamdy120@hotmail.com>
2026-04-13 15:14:43 +0200

    Enhancing the function by eliminationg if statements

--- monitor.sh
+++ monitor.sh
@@ -16,38 +16,12 @@ touch "$dir/${FILENAME}"
 log_with_label() {
 
 local label="$1"
-local command="$2"
-{
-echo "==================='$label'=================="
-if [ "$command" = "date" ]; then 
-date | while IFS= read -r line; do
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] $line "
-done
-
-elif [ "$command" = "uptime" ]; then
-uptime | while IFS= read -r line; do
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] $line "
-done
-
-elif [ "$command" = "df -h | head -1 && df -h | grep '/dev/sda2'" ]; then
-(df -h | head -1 && df -h | grep '/dev/sda2') | while IFS= read -r line; do
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] $line "
-done
-usage=$(df -h | grep '/dev/sda2' | awk '{print $5}' | tr -d '%')
-if [ "$usage" -gt 20 ]; then
-echo "[$(date '+%Y-%m-%d %H:%M:%S')]  Warning: the disk usage exceeds 20%"
-fi
-
-elif [ "$command" = "free -h" ]; then
-free -h | while IFS= read -r line; do
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] $line "
-
-done
-fi
-
-} >> "$dir/${FILENAME}" 
+shift
+echo "============================'$label'=========================" >> "$dir/${FILENAME}"
+eval "$@" | while IFS= read -r line; do
+echo "[ $(date '+%Y-%m-%d %H %M %S') ] $line "
+done >> "$dir/${FILENAME}"
 }
-
 log_with_label "Server date" "date"
 log_with_label "Server uptime" "uptime"
 log_with_label "Disk uasge" "df -h | head -1 && df -h | grep '/dev/sda2'"
