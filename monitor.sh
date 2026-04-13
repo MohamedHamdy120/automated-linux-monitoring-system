2dbfabc174f5f49f72db1665a01cad945d60b5e0
Mohamed Hamdy <mohamedhamdy120@hotmail.com>
2026-04-13 19:12:50 +0200

    Testing

--- monitor.sh
+++ monitor.sh
@@ -21,6 +21,14 @@ echo "============================'$label'=========================" >> "$dir/${
 eval "$@" | while IFS= read -r line; do
 echo "[ $(date '+%Y-%m-%d %H %M %S') ] $line "
 done >> "$dir/${FILENAME}"
+} 
+monitor_mem_usage(){
+echo "============================Memory usage=========================" >> "$dir/${FILENAME}"
+local mem_total=$(grep MemTotal /proc/meminfo | awk '{print $2}')
+local mem_available=$(grep MemAvailable /proc/meminfo | awk '{print $2}')
+local mem_used=$((mem_total - mem_available))
+local usage=$((mem_used * 100 / mem_total))
+echo "[ $(date '+%Y-%m-%d %H %M %S') ] the memory usage is $usage%" >> "$dir/${FILENAME}"
 }
 disk_usage_alert(){
 local usage="$1"
@@ -28,8 +36,9 @@ if [ "$usage" -gt 10 ]; then
 echo "Warning: The disk usage exceeds 10%" >> "$dir/${FILENAME}"
 fi
 }
+
 log_with_label "Server date" "date"
 log_with_label "Server uptime" "uptime"
-log_with_label "Memeory usage" "free -h"
-log_with_label "Disk uasge" "df -h /"
+monitor_mem_usage
+log_with_label "Disk usage" "df -h /"
 disk_usage_alert "$(df -h / | awk 'NR==2 {print $5}' | tr -d '%')"
